7e93b3e53b2d80172e2afb76df37577f9991a689
b2210356061 <b2210356061@cs.hacettepe.edu.tr>
2021-12-03 15:02:53 +0300

    Completed

--- myExercise.py
+++ myExercise.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
+import sys
 
+students = sys.argv[2].split(',')
+universityOf = dict()
+with open(sys.argv[1], 'r') as f:
+	for line in f:
+		universityOf[line.split(':')[0]] = line.split(':')[1].strip()
 
+for student in students:
+	try:
+		print(f"Name: {student}, University: {universityOf[student]}")
+	except:
+		print(f"No record of {student} was found!")
